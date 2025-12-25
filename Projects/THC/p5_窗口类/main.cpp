2bec71e0bc63ed592253bfa597f94fb2d41b7cd3
Huichang Tang <15180887+ztcsone@user.noreply.gitee.com>
2025-12-25 23:15:13 +0800

    增加 window类

---
+++ Projects/THC/p5_窗口类/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "window.h"
+#include <iostream>
+#include <string>
+
+int main() {
+	Window window();
+	return 0;
+
+}
