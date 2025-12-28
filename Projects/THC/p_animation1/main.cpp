7006d469c511f008afdadcb072b319e5f043f6f4
Huichang Tang <15180887+ztcsone@user.noreply.gitee.com>
2025-12-28 12:36:51 +0800

    增加模型

--- Projects/THC/p_animation1/main.cpp
+++ Projects/THC/p_animation1/main.cpp
@@ -25,7 +25,7 @@ int main() {
 
 	// 加载模型（骨骼）与动画（从同一 glb 文件读取）
 	Model model(rootURL + "resources/model/sci-fi_solder/sci-fi_solder.glb");
-	Animation animation(rootURL + "resources/model/sci - fi_solder/sci-fi_solder.glb", &model);
+	Animation animation(rootURL + "resources/model/sci-fi_solder/sci-fi_solder.glb", &model);
 	Animator animator(&animation);
 
 	// 着色器预设（投影可在窗口大小变化时更新）
