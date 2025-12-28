a9e6a59f6248c4293e2f01cc0a8ec92b6b9691eb
Huichang Tang <15180887+ztcsone@user.noreply.gitee.com>
2025-12-28 14:01:50 +0800

    创建新方案

--- src/custom/Projection.cpp
+++ src/custom/Projection.cpp
@@ -1,4 +1,4 @@
-#include <custom/Projection.h>
+ï»¿#include <custom/Projection.h>
 using namespace std;
 
 glm::mat4 Projection::getProjection() {
@@ -7,24 +7,3 @@ glm::mat4 Projection::getProjection() {
     return ProjectionMat;
 }
 
-int main() {
-    if (!glfwInit()) {
-        cout << "GLFW³õÊ¼»¯Ê§°Ü" <<" ";
-        return -1;
-    }
-    Projection proj(60.0f, 0.1f, 100.0f, 800, 600);
-    glm::mat4 useMat = proj.getProjectionMat();
-    proj.setFOV(70.0f); 
-    glm::mat4 newMat = proj.getProjectionMat(); 
-    float* matData = (float*)glm::value_ptr(newMat); 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 4; j++) {
-            cout << matData[j * 4 + i] << "\t"; 
-        }
-        cout << "\n";
-    }
-    proj.setFOV(75.0f);
-    glm::mat4 newProjectionMat = proj.getProjectionMat(); 
-    glfwTerminate();
-    return 0;
-}
