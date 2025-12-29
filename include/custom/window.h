2e79a7597e900c0bca0ab6fcfe1008e41ceb547a
Huichang Tang <15180887+ztcsone@user.noreply.gitee.com>
2025-12-29 22:27:58 +0800

    window类默认抗锯齿

--- include/custom/window.h
+++ include/custom/window.h
@@ -46,6 +46,7 @@ private:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_SAMPLES, 4); // 4x超采样
     }
 
     void createWindow(int w, int h, const char* title, bool getMouse = true) {
